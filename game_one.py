9815e0c76d7b91a547f349fc13253353a559254c
David Wakeley <devyeti1337@gmail.com>
2022-03-18 15:16:15 -0600

    added planet one

--- game_one.py
+++ game_one.py
@@ -6,9 +6,11 @@ import pygame
 screen_size = [360, 600]
 screen = pygame.display.set_mode(screen_size)
 background = pygame.image.load('background.png')
+planet = pygame.image.load('p_one.png')
 
 keep_alive = True
 #game loop
 while keep_alive:
     screen.blit(background, [0, 0])
     pygame.display.update()
+    screen.blit(planet, [140, 50])
